ebc2625cb84af7ecdefdc6f100f1499437cefb65
Benjamin Toh <toh.benjamin123@gmail.com>
2023-05-12 09:31:09 +0800

    create anchors

--- anchor 2/anchor 2.ino
+++ anchor 2/anchor 2.ino
@@ -4,15 +4,8 @@ For ESP32 UWB or ESP32 UWB Pro
 
 */
 #include <SPI.h>
-#include <DW1000.h>
-#include <DW1000CompileOptions.h>
-#include <DW1000Constants.h>
-#include <DW1000Device.h>
-#include <DW1000Mac.h>
-#include <DW1000Ranging.h>
-#include <DW1000Time.h>
-#include <deprecated.h>
-#include <require_cpp11.h>
+#include "DW1000Ranging.h"
+#include "DW1000.h"
 
 #include <WiFi.h>
 #include "link.h"
@@ -30,42 +23,37 @@ const uint8_t PIN_RST = 27; // reset pin
 const uint8_t PIN_IRQ = 34; // irq pin
 const uint8_t PIN_SS = 4;   // spi select pin
 
-const char *ssid = "TP-Link_3logytech";
-const char *password = "3logytech1928";
-const char *host = "192.168.0.143";
-WiFiClient client;
-
 struct MyLink *uwb_data;
 int index_num = 0;
 long runtime = 0;
 String all_json = "";
 
-uint16_t Adelay = 16267;
+uint16_t Adelay = 16397;
 float dist_m = 0.8; //meters
 
 void setup() {
   Serial.begin(115200);
 
-  WiFi.mode(WIFI_STA);
-  WiFi.setSleep(false);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("Connected");
-  Serial.print("IP Address:");
-  Serial.println(WiFi.localIP());
+  // WiFi.mode(WIFI_STA);
+  // WiFi.setSleep(false);
+  // WiFi.begin(ssid, password);
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(500);
+  //   Serial.print(".");
+  // }
+  // Serial.println("Connected");
+  // Serial.print("IP Address:");
+  // Serial.println(WiFi.localIP());
 
-  if (client.connect(host, 80)) {
-    Serial.println("Success");
-    client.print(
-      String("GET /") + " HTTP/1.1\r\n" +
-      "Host: " + host + "\r\n" +
-      "Connection: close\r\n" +
-      "\r\n"
-    );
-  }
+  // if (client.connect(host, 80)) {
+  //   Serial.println("Success");
+  //   client.print(
+  //     String("GET /") + " HTTP/1.1\r\n" +
+  //     "Host: " + host + "\r\n" +
+  //     "Connection: close\r\n" +
+  //     "\r\n"
+  //   );
+  // }
 
   delay(1000);
   Serial.println("Anchor config and start");
@@ -85,28 +73,28 @@ void setup() {
   DW1000Ranging.attachBlinkDevice(newBlink);
   DW1000Ranging.attachInactiveDevice(inactiveDevice);
   //Enable the filter to smooth the distance
-  //DW1000Ranging.useRangeFilter(true);
+  // DW1000Ranging.useRangeFilter(true);
 
   //we start the module as an anchor
   // DW1000Ranging.startAsAnchor("82:17:5B:D5:A9:9A:E2:9C", DW1000.MODE_LONGDATA_RANGE_ACCURACY);
 
-  // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
+  DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
   // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_SHORTDATA_FAST_LOWPOWER);
-  DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_FAST_LOWPOWER, false);
+  // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_FAST_LOWPOWER, false);
   // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_SHORTDATA_FAST_ACCURACY);
   // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_FAST_ACCURACY, false);
   // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_RANGE_ACCURACY);
-  uwb_data = init_link();
+  // uwb_data = init_link();
 }
 
 void loop() {
   DW1000Ranging.loop();
 
-  if ((millis() - runtime) > 1000) {
-    make_link_json(uwb_data, &all_json);
-    send_udp(&all_json);
-    runtime = millis();
-  }
+  // if ((millis() - runtime) > 1000) {
+  //   make_link_json(uwb_data, &all_json);
+  //   send_udp(&all_json);
+  //   runtime = millis();
+  // }
 }
 
 void newRange() {
@@ -128,12 +116,12 @@ void newRange() {
   Serial.print(DW1000Ranging.getDistantDevice()->getRXPower());
   Serial.println(" dBm");
 
-  fresh_link(
-    uwb_data, 
-    DW1000Ranging.getDistantDevice()->getShortAddress(), 
-    DW1000Ranging.getDistantDevice()->getRange(), 
-    DW1000Ranging.getDistantDevice()->getRXPower()
-  );
+  // fresh_link(
+  //   uwb_data, 
+  //   DW1000Ranging.getDistantDevice()->getShortAddress(), 
+  //   DW1000Ranging.getDistantDevice()->getRange(), 
+  //   DW1000Ranging.getDistantDevice()->getRXPower()
+  // );
 }
 
 void newBlink(DW1000Device *device) {
@@ -141,19 +129,19 @@ void newBlink(DW1000Device *device) {
   Serial.print(" short:");
   Serial.println(device->getShortAddress(), HEX);
 
-  add_link(uwb_data, device->getShortAddress());
+  // add_link(uwb_data, device->getShortAddress());
 }
 
 void inactiveDevice(DW1000Device *device) {
   Serial.print("delete inactive device: ");
   Serial.println(device->getShortAddress(), HEX);
 
-  delete_link(uwb_data, device->getShortAddress());
+  // delete_link(uwb_data, device->getShortAddress());
 }
 
-void send_udp(String *msg_json) {
-  if (client.connected()) {
-    client.print(*msg_json);
-    Serial.println("UDP send");
-  }
-}
+// void send_udp(String *msg_json) {
+//   if (client.connected()) {
+//     client.print(*msg_json);
+//     Serial.println("UDP send");
+//   }
+// }
